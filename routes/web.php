1d1c8a16d5ea597fa344c941618e49deb3459e86
RajukrRaja <rajukumar.191813@gmail.com>
2026-04-19 08:18:25 +0530

    save main changes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
-    return view('welcome');
+    try {
+        DB::connection()->getPdo();
+        $status = 'connected';
+    } catch (\Exception $e) {
+        $status = 'error';
+    }
+
+    return view('welcome', compact('status'));
 });
